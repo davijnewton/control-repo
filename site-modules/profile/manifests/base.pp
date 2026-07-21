09d06088c6850879a0666d50767e89d787efdde9
davidjn <david.newton@perforce.com>
2026-07-21 16:27:11 +0100

    Add motd module configuration to base profile

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,9 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
   notify { 'This is the base profile. It should be included on all nodes.': }
+
+  # Configure message of the day
+  class { 'motd':
+    ensure => present,
+  }
 }
