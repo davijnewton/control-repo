f2602da603a8e05fa443ee40c5dfb96db10a01c6
davidjn <david.newton@perforce.com>
2026-07-21 16:31:22 +0100

    Add message of the day with system information

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,17 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
   notify { 'This is the base profile. It should be included on all nodes.': }
+
+  # Configure message of the day with system information
+  class { 'motd':
+    ensure  => present,
+    content => @(EOT),
+      ========================================
+      Welcome to ${facts['hostname']}
+      OS: ${facts['os']['name']} ${facts['os']['release']['major']}
+      IP Address: ${facts['networking']['ip']}
+      Managed by Puppet Infrastructure
+      ========================================
+      | EOT
+  }
 }
