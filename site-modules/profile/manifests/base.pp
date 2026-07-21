fc033b4129719bcab8b3a184306aed96d213aca6
davidjn <david.newton@perforce.com>
2026-07-21 16:11:33 +0100

    Add motd module and configure in base profile

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,9 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
   notify { 'This is the base profile. It should be included on all nodes.': }
+
+  # Configure message of the day
+  class { 'motd':
+    content => "Welcome to ${facts['hostname']}\nManaged by Puppet\n",
+  }
 }
