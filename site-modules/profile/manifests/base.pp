535951faee08691a3058474de8982fe1a3c619d4
davidjn <david.newton@perforce.com>
2026-07-22 15:21:31 +0100

    Add myapp demo module to base profile

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,6 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base {
+  include myapp
+
   notify { 'This is the base profile. It should be included on all nodes.': }
 }
--- site-modules/profile/spec/classes/base_spec.rb
+++ site-modules/profile/spec/classes/base_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 
 describe 'profile::base' do
+  let(:pre_condition) { 'class myapp {}' }
+
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
